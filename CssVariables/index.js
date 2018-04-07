29c4a997ecc14f501ff6133ffd3aa0eadfef1693
Samuel SIlva <samuelsilvawb@gmail.com>
2018-04-07 09:20:08 -0300

    feat : create logic to change values
    - create logic to handle with all controls

--- CssVariables/index.js
+++ CssVariables/index.js
@@ -1,3 +1,26 @@
 (function (window, document) {
-    console.log('started');    
+    'use strict';
+
+    const _VARIABLE_BLUR = '--blur';
+    const _VARIABLE_BACKGROUND = '--background-color';
+    const _VARIABLE_SPACING = '--spacing';
+    const types = {
+        'background': _VARIABLE_BACKGROUND,
+        'blur': _VARIABLE_BLUR,
+        'spacing': _VARIABLE_SPACING
+    };
+    const jsImageStyling = document.querySelector('.js-image-styling');
+    const jsInputsControls = Array.prototype.slice.call(document.querySelectorAll('.js-input-control'));
+    const handleChangeInput = function (e) {
+        if (this === window) {
+            console.error('Object "this" is appointing to windows');
+            return;
+        }
+        const valueToApply = this.value;
+        const whereApply = this.dataset.type;
+        jsImageStyling.style.setProperty(types[whereApply], `${valueToApply}px`)
+    };
+
+    jsInputsControls.forEach(input => input.addEventListener('change', handleChangeInput, false))
+
 })(window, document);
