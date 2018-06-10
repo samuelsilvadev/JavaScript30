efc00ccc966c6a96fda114fb0aaba8e325e1ae50
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-10 19:10:13 -0300

    feat : add controls on board
    - added color selection
    - added width paint

--- Canvas-HTML5/index.js
+++ Canvas-HTML5/index.js
@@ -1,21 +1,28 @@
 'use strict';
 (function (win, $) {
     const $canvas = $.querySelector('[data-js="canvas"]');
+    const $colorPincel = $.querySelector('[data-js="color-pincel"]');
+    const $colorPincelColoful = $.querySelector('[data-js="color-pincel-colorful"]');
+    const $widthPincel = $.querySelector('[data-js="width-pincel"]');
     const ctx = $canvas.getContext('2d');
+    let isDrawing = false;
+    let x = 0;
+    let y = 0;
+    let degColor = 0;
+    let colorful = $colorPincelColoful.checked;
+
     $canvas.width = win.innerWidth - 20;
     $canvas.height = win.innerHeight - 20;
     ctx.strokeStyle = '#c2c2c2';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
     ctx.lineWidth = 30;
-    let isDrawing = false;
-    let x = 0;
-    let y = 0;
-    let degColor = 0;
+
     $canvas.addEventListener('mousemove', function draw(e) {
         if (isDrawing) {
-            console.log(e);
-            ctx.strokeStyle = `hsl(${degColor}, 100%, 50%)`;
+            if (colorful) {
+                ctx.strokeStyle = `hsl(${degColor}, 100%, 50%)`;
+            }
             ctx.beginPath();
             ctx.moveTo(x, y);
             ctx.lineTo(e.offsetX, e.offsetY);
@@ -24,10 +31,24 @@
             degColor++;
         }
     });
+
     $canvas.addEventListener('mousedown', (e) => {
         isDrawing = true;
         [x, y] = [e.offsetX, e.offsetY];
     });
+
     $canvas.addEventListener('mouseup', () => isDrawing = false);
+
     $canvas.addEventListener('mouseout', () => isDrawing = false);
+
+    $colorPincel.addEventListener('change', (e) => {
+        ctx.strokeStyle = e.target.value;
+        colorful = false;
+    });
+
+    $colorPincelColoful.addEventListener('click', (e) => colorful = $colorPincelColoful.checked);
+
+    $widthPincel.addEventListener('change', (e) => ctx.lineWidth = e.target.value);
+
+    $.addEventListener('DOMContentLoaded', () => ctx.strokeStyle = $colorPincel.value);
 })(window, document);
