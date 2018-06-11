9d24b909cffd3cbdefc54b96c6127cebe3597eaf
Samuel SIlva <samuelsilvawb@gmail.com>
2018-06-11 18:55:39 -0300

    refactor : improve code
    - improved js code
    - added feature to hide pallete when user is drawing

--- Canvas-HTML5/index.js
+++ Canvas-HTML5/index.js
@@ -4,6 +4,7 @@
     const $colorPincel = $.querySelector('[data-js="color-pincel"]');
     const $colorPincelColoful = $.querySelector('[data-js="color-pincel-colorful"]');
     const $widthPincel = $.querySelector('[data-js="width-pincel"]');
+    const $pallete = $.querySelector('[data-js="pallete"]');
     const ctx = $canvas.getContext('2d');
     let isDrawing = false;
     let x = 0;
@@ -11,44 +12,67 @@
     let degColor = 0;
     let colorful = $colorPincelColoful.checked;
 
-    $canvas.width = win.innerWidth - 20;
-    $canvas.height = win.innerHeight - 20;
-    ctx.strokeStyle = '#c2c2c2';
-    ctx.lineJoin = 'round';
-    ctx.lineCap = 'round';
-    ctx.lineWidth = 30;
+    function initDefaultValues() {
+        $canvas.width = win.innerWidth - 20;
+        $canvas.height = win.innerHeight - 20;
+        ctx.strokeStyle = '#c2c2c2';
+        ctx.lineJoin = 'round';
+        ctx.lineCap = 'round';
+        ctx.lineWidth = 30;
+    }
 
-    $canvas.addEventListener('mousemove', function draw(e) {
-        if (isDrawing) {
-            if (colorful) {
-                ctx.strokeStyle = `hsl(${degColor}, 100%, 50%)`;
+    function initEvents() {
+
+        $canvas.addEventListener('mousemove', function draw(e) {
+            if (isDrawing) {
+                hidePallete();
+                if (colorful) {
+                    ctx.strokeStyle = `hsl(${degColor}, 100%, 50%)`;
+                }
+                ctx.beginPath();
+                ctx.moveTo(x, y);
+                ctx.lineTo(e.offsetX, e.offsetY);
+                ctx.stroke();
+                [x, y] = [e.offsetX, e.offsetY];
+                degColor++;
             }
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            ctx.lineTo(e.offsetX, e.offsetY);
-            ctx.stroke();
+        });
+
+        $canvas.addEventListener('mousedown', (e) => {
+            isDrawing = true;
             [x, y] = [e.offsetX, e.offsetY];
-            degColor++;
-        }
-    });
+        });
 
-    $canvas.addEventListener('mousedown', (e) => {
-        isDrawing = true;
-        [x, y] = [e.offsetX, e.offsetY];
-    });
+        $canvas.addEventListener('mouseup', () => {
+            isDrawing = false;
+            showPallete();
+        });
 
-    $canvas.addEventListener('mouseup', () => isDrawing = false);
+        $canvas.addEventListener('mouseout', () => {
+            isDrawing = false;
+            showPallete();
+        });
 
-    $canvas.addEventListener('mouseout', () => isDrawing = false);
+        $colorPincel.addEventListener('change', (e) => {
+            ctx.strokeStyle = e.target.value;
+            colorful = false;
+        });
 
-    $colorPincel.addEventListener('change', (e) => {
-        ctx.strokeStyle = e.target.value;
-        colorful = false;
-    });
+        $colorPincelColoful.addEventListener('click', (e) => colorful = $colorPincelColoful.checked);
+        $widthPincel.addEventListener('change', (e) => ctx.lineWidth = e.target.value);
+        $.addEventListener('DOMContentLoaded', () => ctx.strokeStyle = $colorPincel.value);
+    }
 
-    $colorPincelColoful.addEventListener('click', (e) => colorful = $colorPincelColoful.checked);
+    function hidePallete() {
+        $pallete.style.opacity = 0;
+        $pallete.style.display = 'none';
+    }
 
-    $widthPincel.addEventListener('change', (e) => ctx.lineWidth = e.target.value);
+    function showPallete() {
+        $pallete.style.display = 'block';
+        $pallete.style.opacity = 1;
+    }
 
-    $.addEventListener('DOMContentLoaded', () => ctx.strokeStyle = $colorPincel.value);
+    initDefaultValues();
+    initEvents();
 })(window, document);
