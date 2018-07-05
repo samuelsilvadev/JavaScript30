d326ca6dbe6b454dec023da375590d481d381ed4
Samuel SIlva <samuelsilvawb@gmail.com>
2018-07-05 20:06:01 -0300

    feat : create logic to select checks from up to bottom
    - created logic to select checks when shift is pressed, up to bottom.

--- Multiple-check/index.js
+++ Multiple-check/index.js
@@ -1,4 +1,30 @@
 'use strict';
 (function () {
 
+    const $checkboxes = document.querySelectorAll('[data-js="box-todo-item"]');
+    const arrayChecks = Array.from($checkboxes);
+    let firstClick = 0;
+    let lastClick = 0
+    let shiftIsPressed = false;
+
+    arrayChecks.forEach((check, index) => {
+        check.addEventListener('click', e => {
+            if (shiftIsPressed) {
+                lastClick = index;
+            } else {
+                firstClick = index;
+            }
+            if (firstClick >= 0 && lastClick > 0) {
+                arrayChecks.slice(firstClick, lastClick).forEach(c => c.checked = true);
+                shiftIsPressed = false;
+            }
+        });
+    });
+
+    document.addEventListener('keydown', e => {
+        if (e.shiftKey) {
+            shiftIsPressed = true;
+        }
+    });
+
 })();
