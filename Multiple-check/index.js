87caa2fc67269784f3f460d9dfcb9dbdde127c52
Samuel SIlva <samuelsilvawb@gmail.com>
2018-07-06 10:04:42 -0300

    refactor : improve code
    - improved code, breaked into smaller functions

--- Multiple-check/index.js
+++ Multiple-check/index.js
@@ -1,5 +1,5 @@
 'use strict';
-(function () {
+(function (window, document) {
 
     const $checkboxes = document.querySelectorAll('[data-js="box-todo-item"]');
     const arrayChecks = Array.from($checkboxes);
@@ -9,22 +9,7 @@
 
     arrayChecks.forEach((check, index) => {
         check.addEventListener('click', e => {
-            if (shiftIsPressed) {
-                lastClick = index;
-            } else {
-                firstClick = index;
-            }
-
-            if (isNotNull(firstClick) && isNotNull(lastClick) && firstClick !== lastClick) {
-
-                const one = firstClick > lastClick ? lastClick : firstClick;
-                const last = lastClick > firstClick ? lastClick : firstClick;
-
-                arrayChecks.slice(one, last).forEach(c => c.checked = true);
-                shiftIsPressed = false;
-                firstClick = null;
-                lastClick = null;
-            }
+            handleCheck(e, index);
         });
     });
 
@@ -37,4 +22,24 @@
     function isNotNull(value) {
         return value !== null;
     }
-})();
+
+    function handleCheck(event, index) {
+        if (shiftIsPressed) {
+            lastClick = index;
+        } else {
+            firstClick = index;
+        }
+
+        if (isNotNull(firstClick) && isNotNull(lastClick) && firstClick !== lastClick) {
+
+            const one = firstClick > lastClick ? lastClick : firstClick;
+            const last = lastClick > firstClick ? lastClick : firstClick;
+
+            arrayChecks.slice(one, last).forEach(c => c.checked = true);
+            shiftIsPressed = false;
+            firstClick = null;
+            lastClick = null;
+        }
+    }
+
+})(window, document);
