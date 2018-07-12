672e5965205d598573e109b7ad9f9c3d2b35942f
Samuel SIlva <samuelsilvawb@gmail.com>
2018-07-12 07:04:42 -0300

    feat : create box to set text is typing
    - created box
    - added text inside

--- Sequence-of-Code/index.js
+++ Sequence-of-Code/index.js
@@ -1,15 +1,23 @@
 'use strict';
 (function (win, doc) {
 
+    const $boxToText = doc.querySelector('[data-js="box-typing"]');
     const pressedKeys = [];
     const SECRET_KEY = 'ismagic';
 
-    win.addEventListener('keyup', e => {        
+    function setTextinBox() {
+        $boxToText.textContent = pressedKeys.join('');
+    }
+
+    win.addEventListener('keyup', e => {
         pressedKeys.push(e.key);
         pressedKeys.splice(-SECRET_KEY.length - 1, pressedKeys.length - SECRET_KEY.length);
-        if(pressedKeys.join('').indexOf(SECRET_KEY) > -1) {
+        setTextinBox();
+        if (pressedKeys.join('').indexOf(SECRET_KEY) > -1) {
             console.log('YOU WON');
-        }        
+        }
     });
+
+
 
 })(window, document);
