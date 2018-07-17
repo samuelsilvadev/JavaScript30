64195e01761f4cfa9cbb22cda782d3e0184707f3
Samuel SIlva <samuelsilvawb@gmail.com>
2018-07-17 20:07:16 -0300

    feat : add control to show new foods
    - created method to add new food
    - updating list after insert new food

--- LocalStorage-EventDelegation/index.js
+++ LocalStorage-EventDelegation/index.js
@@ -1,4 +1,34 @@
 'use strict';
 (function (win, doc) {
 
+    const foods = [];
+    const $formAddFood = doc.querySelector('[data-js="form-add-food"]');
+    const $listFoods = doc.querySelector('[data-js="list-foods"]');
+
+    function addFood(e) {
+        e.preventDefault();
+        const food = {
+            name: (this.querySelector('[data-js="input-food"]')).value,
+            selected: false,
+        };
+        foods.push(food);
+        this.reset();
+    }
+
+    function populateList(foods = [], $list) {
+        $list.innerHTML = foods.map((food, i) => (
+            `
+            <li>
+                <input id="${i}" type="checkbox" ${food.selected ? 'checked' : ''}">
+                <label for="${i}">${food.name}</label>
+            </li>
+            `
+        )).join('');
+    }
+
+    $formAddFood.addEventListener('submit', function (e) {
+        addFood.bind(this)(e);
+        populateList(foods, $listFoods);
+    });
+
 })(window, document);
