6c9ce17fba9e04e35141fa4f1e54a4786e38797a
Samuel SIlva <samuelsilvawb@gmail.com>
2018-07-20 10:29:01 -0300

    feat : add dynamic text when mouse move
    - added event js to control shadow
    - added css to positionated elements on view

--- Text-Shadow/index.js
+++ Text-Shadow/index.js
@@ -1,4 +1,25 @@
 'use strict';
-(function () {
+(function (win, doc) {
+    const $box = doc.querySelector('[data-js="box"]');
+    const $boxText = doc.querySelector('[data-js="box-text"]');
+    const walk = 20;
 
-});
+    function handleCreateShadow(e) {
+        const { offsetWidth: width, offsetHeight: height } = $box;
+        let { offsetX: x, offsetY: y } = e;
+        if (this !== e.target) {
+            x += e.target.offsetLeft;
+            y += e.target.offsetTop;
+        }
+
+        const xWalk = Math.round((x / width * walk) - (walk / 2));
+        const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+        $boxText.style.textShadow = `
+            ${xWalk}px ${yWalk}px 0 red
+        `;
+    }
+
+    $box.addEventListener('mousemove', handleCreateShadow);
+
+})(window, document);
