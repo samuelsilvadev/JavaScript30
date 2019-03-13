3465af3163d9f1bab4ca3b6943f2d8b760baed63
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-13 20:33:10 +0100

    feat(webcam-fun) : add video in canvas
    - Removed unnecessary `src` attribute from video in html.
    - Added basic css to centralize canvas and video.
    - Added in canvas video image.

--- Webcam-Fun/index.js
+++ Webcam-Fun/index.js
@@ -1,14 +1,17 @@
 'use strict';
 
 (function() {
+    const $canvas = document.querySelector('[data-js="canvas"');
     const $video = document.querySelector('[data-js="video"');
+    const canvasContext = $canvas.getContext('2d');
 
     function init() {
-        getVideo();
+        getVideo()
+            .then(paintToCanvas);
     }
 
     function getVideo() {
-        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        return navigator.mediaDevices.getUserMedia({ video: true, audio: false })
             .then((localMediaScreen) => {
                 try {
                     /**
@@ -19,11 +22,24 @@
                 } catch(error) {
                     $video.srcObject = localMediaScreen;
                 }
-                $video.play();
+                return $video.play();
             })
             .catch((error) => {
                 console.error('Something went wrong', error);
             });
+    }
+
+    function paintToCanvas() {
+        const { videoWidth, videoHeight } = $video;
+		
+        $canvas.width = videoWidth;
+        $canvas.height = videoHeight;
+
+        const intervalId = setInterval(() => {
+            canvasContext.drawImage($video, 0, 0, videoWidth, videoHeight);
+        }, 16);
+
+        return intervalId;
     }
 
     init();
