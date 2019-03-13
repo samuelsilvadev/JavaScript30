ba4b83f0ffd49d64ae6aa277113c7b3963f2b228
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-13 21:10:42 +0100

    feat(webcam-fun) : add button to take a photo and create link to download further
    - Created button and added listener to take a photo.
    - Created dom element to keep all generated links with photos.

--- Webcam-Fun/index.js
+++ Webcam-Fun/index.js
@@ -3,10 +3,13 @@
 (function() {
     const $canvas = document.querySelector('[data-js="canvas"');
     const $video = document.querySelector('[data-js="video"');
+    const $strip = document.querySelector('[data-js="strip"');
+    const $btnTakeAPhoto = document.querySelector('[data-js="btn-take-a-photo"');
     const canvasContext = $canvas.getContext('2d');
 
     function init() {
         $video.addEventListener('canplay', paintToCanvas);
+        $btnTakeAPhoto.addEventListener('click', takeAPhoto);
 
         getVideo();
     }
@@ -41,6 +44,16 @@
         }, 16);
 
         return intervalId;
+    }
+
+    function takeAPhoto() {
+        const data = $canvas.toDataURL('image/jpeg');
+        const $link = document.createElement('a');
+        
+        $link.href = data;
+        $link.textContent = 'Download Image';
+
+        $strip.insertBefore($link, $strip.firstChild);
     }
 
     init();
