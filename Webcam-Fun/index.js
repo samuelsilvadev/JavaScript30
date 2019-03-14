73399980cb44e65378ec38f64a34325e4ebcdf91
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-14 22:40:03 +0100

    feat(webcam-fun) : create form to apply effect
    - Created form to greenEffect.
    - Created buttons to redEffect and rgbsplitEffect.

--- Webcam-Fun/index.js
+++ Webcam-Fun/index.js
@@ -1,15 +1,29 @@
 'use strict';
 
 (function() {
-    const $canvas = document.querySelector('[data-js="canvas"');
-    const $video = document.querySelector('[data-js="video"');
-    const $strip = document.querySelector('[data-js="strip"');
-    const $btnTakeAPhoto = document.querySelector('[data-js="btn-take-a-photo"');
+    const EFFECTS = {
+        NONE: 0,
+        RED_EFFECT: 1,
+        RGB_SPLIT: 2,
+        GREEN_EFFECT: 3,
+    };
+    const $canvas = document.querySelector('[data-js="canvas"]');
+    const $video = document.querySelector('[data-js="video"]');
+    const $strip = document.querySelector('[data-js="strip"]');
+    const $btnTakeAPhoto = document.querySelector('[data-js="btn-take-a-photo"]');
+    const $btnGreenEffect = document.querySelector('[data-js="btn-green-effect"]');
+    const $btnRedEffect = document.querySelector('[data-js="btn-red-effect"]');
+    const $btnRgbSplitEffect = document.querySelector('[data-js="btn-rgb-split-effect"]');
+    const $formGreenEffect = document.querySelector('[data-js="green-effect-form"]');
     const canvasContext = $canvas.getContext('2d');
+    let currentEffect = EFFECTS.NONE;
 
     function init() {
         $video.addEventListener('canplay', paintToCanvas);
         $btnTakeAPhoto.addEventListener('click', takeAPhoto);
+        $btnGreenEffect.addEventListener('click', changeEffect(EFFECTS.GREEN_EFFECT));
+        $btnRedEffect.addEventListener('click', changeEffect(EFFECTS.RED_EFFECT));
+        $btnRgbSplitEffect.addEventListener('click', changeEffect(EFFECTS.RGB_SPLIT));
 
         getVideo();
     }
@@ -41,6 +55,25 @@
 
         const intervalId = setInterval(() => {
             canvasContext.drawImage($video, 0, 0, videoWidth, videoHeight);
+
+            let pixels = canvasContext.getImageData(0, 0, videoWidth, videoHeight);
+            
+            switch(currentEffect) {
+                case EFFECTS.RED_EFFECT:
+                    pixels = webcamEffects.redEffect(pixels);
+                    break;
+                case EFFECTS.RGB_SPLIT:
+                    pixels = webcamEffects.rgbSplit(pixels);
+                    break;
+                case EFFECTS.GREEN_EFFECT:
+                    const levels = generateLevels();
+                    pixels = webcamEffects.greenEffect(pixels, levels);
+                    break;
+                default:
+                    break;
+            }
+
+            canvasContext.putImageData(pixels, 0, 0);
         }, 16);
 
         return intervalId;
@@ -57,10 +90,29 @@
         $strip.insertBefore($link, $strip.firstChild);
     }
 
+    function generateLevels() {
+        const levels = Array.from($formGreenEffect.elements)
+            .reduce((levels, current) => {
+                if (current.id) {
+                    levels[current.name] = parseInt(current.value);
+                }
+
+                return levels;
+            }, {});
+
+        return levels;
+    }
+
     function createThumb(src) {
         return `
             <img class="thumb-image" src="${src}" />
         `;
+    }
+
+    function changeEffect(effect) {
+        return function () {
+            currentEffect = effect;    
+        }
     }
 
     init();
