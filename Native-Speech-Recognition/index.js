ced84240760a09362f831fdcfb3b347806af15e3
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-15 22:40:12 +0100

    chore(native-speech-recognition) : create function to take results while speaking
    - Created setup to start reconize voices.

--- Native-Speech-Recognition/index.js
+++ Native-Speech-Recognition/index.js
@@ -1,3 +1,19 @@
 'use strict';
 
-(function() {})();
+(function() {
+    window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+    function _handleRecognitionSpeech(event) {
+        const results = Array.from(event.results) || [];
+        const groupedResults = results.map(result => result[0]);
+        const transcript = groupedResults.map(result => result.transcript).join('');
+        
+        console.log(transcript);
+    }
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.addEventListener('result', _handleRecognitionSpeech);
+    recognition.addEventListener('end', recognition.start);
+    recognition.start();
+})();
