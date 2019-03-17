8d31201a043daf1ef48a44187f12b0e48c3180a2
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-17 18:55:00 +0100

    feat(native-speech-recognition) : add phrases in view
    - Added results in view.

--- Native-Speech-Recognition/index.js
+++ Native-Speech-Recognition/index.js
@@ -3,17 +3,34 @@
 (function() {
     window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 
-    function _handleRecognitionSpeech(event) {
-        const results = Array.from(event.results) || [];
-        const groupedResults = results.map(result => result[0]);
-        const transcript = groupedResults.map(result => result.transcript).join('');
-        
-        console.log(transcript);
-    }
-
     const recognition = new SpeechRecognition();
+
+    const $wordsContainer = document.querySelector('[data-js="words"]');
+    let $p = createParagraghAndAttachInContainer();
+    
     recognition.interimResults = true;
     recognition.addEventListener('result', _handleRecognitionSpeech);
     recognition.addEventListener('end', recognition.start);
     recognition.start();
+
+    function _handleRecognitionSpeech(event) {
+        const results = Array.from(event.results) || [];
+        const { isFinal } = results[0];
+        const groupedResults = results.map(result => result[0]);
+        const transcript = groupedResults.map(result => result.transcript).join('');
+        
+        $p.textContent = transcript;
+
+        if (isFinal) {
+            $p = createParagraghAndAttachInContainer();
+        }
+    }
+
+    function createParagraghAndAttachInContainer() {
+        let $p = document.createElement('p');
+        $p.setAttribute('class', 'notes__line');
+        $wordsContainer.appendChild($p);
+
+        return $p;
+    }
 })();
