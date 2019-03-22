76098a026656d96269f39a37047063679b68d195
Samuel SIlva <samuelsilvawb@gmail.com>
2019-03-22 21:14:10 +0100

    feat(geolocation-peedometer-compass) : Add control based on geolocation
    - Added `watchPosition` listener.

--- Geolocation-Speedometer-Compass/index.js
+++ Geolocation-Speedometer-Compass/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
-(function() {})();
+(function() {
+    const $compass = document.querySelector('[data-js="compass"]');
+    const $compassSpeedValue = document.querySelector('[data-js="compass-speed-value"]');
+
+    window.navigator.geolocation.watchPosition(_handleWatchPosition);
+
+    function _handleWatchPosition(data) {
+        const hasSpeed = !!data.coords.speed;
+
+        $compassSpeedValue.textContent = hasSpeed ? data.coords.speed : '0';
+        $compass.style.transform = `rotate(${data.coords.heading}deg)`;
+    }
+})();
